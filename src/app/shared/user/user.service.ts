e545e2ae86d38e63e0291db604be67be56d469cc
Ron Mares <ron.mares@mktec.com>
2018-09-26 16:49:09 -0400

    add user service

---
+++ src/app/shared/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from './classes/User';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get('');
+  }
+
+  getById(id: number) {
+    return this.http.get('' + id);
+  }
+  
+  create(user: User) {
+    return this.http.post('', user);
+  }
+
+  update(user: User) {
+    return this.http.put('' + user.id, user);
+  }
+
+  delete(id: number) {
+    return this.http.delete('' + id);
+  }
+
+
+
+}
